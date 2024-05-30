67f8443205970d2944b00e28eb2e70b21152e4b7
Atilio <atilio.jesus@grupoboticario.com.br>
2024-05-30 17:07:24 -0300

    feat: ✨ add separeted modules to get started to work it on

--- src/modules/api/api.controller.ts
+++ src/modules/api/api.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiService } from './api.service';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class ApiController {
+  constructor(private readonly appService: ApiService) {}
 
   @Get()
   getHello(): string {
